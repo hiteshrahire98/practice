b225fa9920a29e8124d3cb1edf109317e5c15113
hiteshahire98 <hituahire1998@gmail.com>
2022-09-23 14:35:55 +0530

    committe

---
+++ practice/Out.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace practice
+//{
+//    internal class Out         //  Out→ you want to out multiple response from the called function / method.
+//    {
+
+//        static void Calculation(int a, int b, out int sum, out int sub, out int multiply)
+//        {
+//            sum = a + b;
+//            sub = a - b;
+//            multiply = a * b;
+
+//        }
+
+//        static void Main(string[] args)
+//        {
+
+//            int a = 10, b = 20, sum, sub, multiply;
+
+//            Calculation(a, b, out sum, out sub, out multiply);
+
+//            Console.WriteLine($"sum= {sum}  sub= {sub} multiplication {multiply}");
+
+//        }
+
+//    }
+//}
---
+++ practice/Params.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace practice
+//{
+//    internal class Params    // Params  → accept variable number of parameters of same data type.
+//    {
+//        static void AcceptNames(params string[] names)
+//        {
+//            for (int i = 0; i < names.Length; i++)
+//            {
+//                Console.WriteLine(names[i]);
+//            }
+//            Console.WriteLine("-------------------");
+//        }
+
+//        static void Main(string[] args)
+//        {
+//            AcceptNames("Kishor", "Rahul", "Amol");
+//            AcceptNames("Tushar");
+//            AcceptNames("Rohan", "Suraj");
+
+//        }
+
+//    }
+//}
