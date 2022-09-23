d0dfac7ff6c0f30f8d5fdf71651daf861d095319
hiteshahire98 <hituahire1998@gmail.com>
2022-09-23 14:20:48 +0530

    c

---
+++ practice/call_by_ref.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace practice
+//{
+//    internal class call_by_ref       // Ref    → call by refference
+//    {
+//        static void Swap(ref int a, ref int b)
+//        {
+//            int temp;
+
+//            temp = a;
+//            a = b;
+//            b = temp;
+//        }
+
+//        static void Main(string[] args)
+//        {
+//            // call by ref
+//            int a = 10, b = 20;
+//            Swap(ref a, ref b);
+//            Console.WriteLine($"a= {a}  b= {b}");
+//        }
+
+//    }
+//}
+
+
+
+
+
+
---
+++ practice/call_by_value.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace practice
+//{
+//    internal class call_by_value
+//    {
+//        static void Swap(int a, int b)
+//        {
+//            int temp;
+
+//            temp = a;
+//            a = b;
+//            b = temp;
+//        }
+
+//        static void Main(string[] args)
+//        {
+//            // call by value
+//            int a = 10, b = 20;
+//            Swap(a,b);
+//            Console.WriteLine($"a= {a}  b= {b}");
+//        }
+//    }
+//}
