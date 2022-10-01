fbc856238ff79a2a5dce41fb746b2a1a80ddf71c
hiteshahire98 <hituahire1998@gmail.com>
2022-10-01 19:02:41 +0530

    commitee

---
+++ practice/SumOfDigit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    internal class SumOfDigit
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0, rem;
+            Console.WriteLine("Enter a number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            while (n > 0)
+            {
+                rem = n % 10;
+                sum= sum + rem;
+                n = n / 10;
+            }
+            Console.WriteLine("Sum is:"+sum);
+
+        }
+    }
+}
---
+++ practice/SwapWithoutThirdVariable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace practice
+//{
+//    internal class SwapWithoutThirdVariable
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Enter A=");
+//            int a=Convert.ToInt32(Console.ReadLine());  //a=5
+//            Console.WriteLine("Enetr B=");
+//            int b=Convert.ToInt32(Console.ReadLine());  //b=15
+
+//            a = a + b;
+//            b = a - b;
+//            a= a - b;
+
+//            Console.WriteLine("a="+a+"\tb="+b);
+//        }
+
+//    }
+//}
